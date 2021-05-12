f16eea6bc5f5f54ebd38948d049636d5a6825fe6
georges-lao <83036696+georges-lao@users.noreply.github.com>
2021-05-12 15:50:40 +0200

    update

--- PowerPlantMangamentAPI/PowerPlantLoadUnitTest/PowerPlantUnitTest.cs
+++ PowerPlantMangamentAPI/PowerPlantLoadUnitTest/PowerPlantUnitTest.cs
@@ -1,18 +1,56 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
 using PowerPlantManagement.Models;
+using System.Collections.Generic;
 
 namespace PowerPlantLoadUnitTest
 {
-    [TestClass]
-    public class PowerPlantUnitTest
-        
+    [TestFixture]
+    public class PowerPlantUnitTest        
     {
-        [TestMethod]
+        [Test]
         public void LoadRequestShould()
         {
+            var intput = new InputPowerPlantDto() { Name = "TestInput"
+                                                   , Efficiency = 100
+                                                   , Pmax = 200
+                                                   , Pmin = 100
+                                                   , Type = "gasfired"
+            };
+            var list = new List<InputPowerPlantDto>();
+            list.Add(intput);
             var test = new LoadRequest(new InputRequestDto() { 
-             Fuels = new InputFuelsDto() { }
-            });
+             Fuels = new InputFuelsDto() { 
+                                            Co2EuroTon = 100,
+                                            gazEeuroMWh = 100,
+                                            KerosineEuroMwh = 100,
+                                            WindIndex = 1
+                                            }
+             , load = 200
+             , InputPowerPlantDtos = list
+             });
+
+            Assert.That(test.GetLoadPlan().Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TurboJetPowerPlantShould ()
+        {
+            var PowerPlant = new TurbojetPowerPlant("TurbojetTest",50,10,1,100);
+            Assert.That(PowerPlant.Profitability, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GasfiredPowerPlantShould()
+        {
+            var PowerPlant = new GasfiredPowerPlant("GazfiredTest", 50, 10, 1, 100);
+            Assert.That(PowerPlant.Profitability, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void WindturbinePowerPlantShould()
+        {
+            var PowerPlant = new WindturbinePowerPlant("WindturbineTest", 100, 10, 1, 100);
+            Assert.That(PowerPlant.Profitability, Is.EqualTo(100));
         }
     }
 }
--- PowerPlantMangamentAPI/PowerPlantManagement/Models/LoadRequest.cs
+++ PowerPlantMangamentAPI/PowerPlantManagement/Models/LoadRequest.cs
@@ -1,8 +1,6 @@
 ﻿using PowerPlantManagement.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace PowerPlantManagement.Models
 {
